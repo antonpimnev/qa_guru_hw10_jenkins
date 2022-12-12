755240445a7ed758366e270706038fbc7b6d8f6f
Anton Pimnev <anton.pimnev@gmail.com>
2022-12-12 21:57:11 +0500

    2

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -16,7 +16,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 10000;
-        Configuration.remote = "https://antonpimnev:1Qwerty!@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
